99675218bc00cc74a0a90d46bf0a24bf746d45c7
Den Rolya <denrolya@gmail.com>
2024-09-23 17:52:45 +0200

    refactor(typo): fixing a typo in model name

--- src/app/daily-ledger/page.tsx
+++ src/app/daily-ledger/page.tsx
@@ -20,7 +20,7 @@ import {
   DrawerTrigger,
 } from '@/components/ui/drawer';
 import { Transaction, Type } from '@/models/transaction.ts';
-import { Transfer } from '@/models/transfer.ts';
+import { Transfer } from '@/models/Transfer';
 import { generateTransactions } from '@/services/transactionGenerator';
 import { generateTransfers } from '@/services/transferGenerator';
 
