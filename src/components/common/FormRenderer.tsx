871d00c614b50c3b24ce368cec79aba6e4cffb41
Den Rolya <denrolya@gmail.com>
2024-09-23 12:15:55 +0200

    fix(layout): fixing layout issues

--- src/components/common/FormRenderer.tsx
+++ src/components/common/FormRenderer.tsx
@@ -146,7 +146,7 @@ export const FormRenderer: React.FC = () => {
 
   return (
     <Drawer open={contextFormState.isOpen} onOpenChange={handleOpenChange}>
-      <DrawerContent className="max-h-[90vh]">
+      <DrawerContent>
         <DrawerHeader className="text-left">
           <DrawerTitle>{title}</DrawerTitle>
         </DrawerHeader>
