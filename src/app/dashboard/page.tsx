e08bdd073be1bee7a7bc3b6616cf5485ddb87824
Den Rolya <denrolya@gmail.com>
2024-10-01 23:13:05 +0200

    refactor(dashboard): remove unnecessary compponents from dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -50,18 +50,7 @@ const DashboardPage: React.FC = () => {
         <div className="col-span-5 md:col-span-3">
           <MoneyFlow />
         </div>
-        <div className="col-span-5 md:col-span-2">
-          <CategoryTreeCard />
-        </div>
       </div>
-      <Card>
-        <CardContent className="p-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <DraftForm />
-            <InputForm />
-          </div>
-        </CardContent>
-      </Card>
     </section>
   );
 };
