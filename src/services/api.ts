bdda3f920fc590230350a6b8b201edbd2651bb4f
Den Rolya <denrolya@gmail.com>
2024-10-05 17:42:04 +0200

    refactor(auth): use localstorage instead of sessionStorage

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ const api: AxiosInstance = axios.create({
 });
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-  const token = sessionStorage.getItem('token');
+  const token = localStorage.getItem('token');
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
   }
