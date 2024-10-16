bc054994e697ed9348218f54a1c38d83af446679
Den Rolya <denrolya@gmail.com>
2024-10-16 13:14:10 +0200

    fix(header): fix displaying decimals in exchange rates in header

--- src/components/layout/ExchangeRatesDetails.tsx
+++ src/components/layout/ExchangeRatesDetails.tsx
@@ -26,7 +26,7 @@ export const ExchangeRatesDetails: React.FC = () => {
           {headerCurrencyPairs.map(({ from, to }) => (
             <div className="font-mono flex flex-col items-center" key={`${from}/${to}`}>
               <span>{from}/{to}</span>
-              <MoneyValue useColors={false} showSymbol={false} amount={getExchangeRate(from, to, fixerExchangeRates) ?? 0} />
+              <MoneyValue useColors={false} showSymbol={false} amount={getExchangeRate(from, to, fixerExchangeRates) ?? 0} currency={to} />
             </div>
           ))}
           <ChevronRight className="h-4 w-4 ml-1" />
